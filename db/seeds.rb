0b42e3248e3d0e489b5c9c4aa35ef35f96037a9b
jeffreytseng3 <jeffrey.tseng3@gmail.com>
2019-11-01 21:53:54 -0400

    update seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -18,23 +18,3 @@ jeffrey = User.create!(
     username: 'jeffreytseng',
     password: 'password'
 )
-
-photo1 = jeffrey.photos.create(title: 'cactus1')
-photo_file1 = open("https://dropify-seeds.s3.amazonaws.com/photo-collection/hannah-crd-8RpP_YwERcU-unsplash.jpg")
-photo1.image_file.attach(io: photo_file1, filename: 'cactus1.jpg')
-photo1.save!
-
-photo2 = jeffrey.photos.create(title: 'cactus2')
-photo_file2 = open("https://dropify-seeds.s3.amazonaws.com/photo-collection/stephanie-harvey-f7PfM2NklZ4-unsplash.jpg")
-photo2.image_file.attach(io: photo_file2, filename: 'cactus2.jpg')
-photo2.save!
-
-photo3 = jeffrey.photos.create(title: 'cactus3')
-photo_file3 = open("https://dropify-seeds.s3.amazonaws.com/photo-collection/thomas-verbruggen-5A06OWU6Wuc-unsplash.jpg")
-photo3.image_file.attach(io: photo_file3, filename: 'cactus3.jpg')
-photo3.save!
-
-photo4 = jeffrey.photos.create(title: 'cactus4')
-photo_file4 = open("https://dropify-seeds.s3.amazonaws.com/photo-collection/ugur-akdemir-xhMTF15IeBw-unsplash.jpg")
-photo4.image_file.attach(io: photo_file4, filename: 'cactus4.jpg')
-photo4.save!
