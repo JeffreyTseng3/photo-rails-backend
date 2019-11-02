f05f3b515997cf9cc351d62b109b88ed8b7bd190
jeffreytseng3 <jeffrey.tseng3@gmail.com>
2019-11-02 13:08:01 -0400

    update

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -9,11 +9,12 @@ Rails.application.config.action_controller.forgery_protection_origin_check = fal
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins /localhost:*/
+    # origins /localhost:*/
+    origins "*"
 
     resource '*',
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head],
-      credentials: true
+      # credentials: true
   end
 end
