2533541639ce1b1203fcd1e872892d3fbfd72bfe
jeffreytseng3 <jeffrey.tseng3@gmail.com>
2019-11-02 00:13:03 -0400

    update

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,14 +2,13 @@
 #
 # Table name: comments
 #
-#  id         :integer          not null, primary key
-#  body       :text             not null
-#  photo_id   :integer
-#  user_id    :integer
-#  created_at :datetime         not null
-#  updated_at :datetime         not null
+#  id           :integer          not null, primary key
+#  comment_body :text             not null
+#  photo_id     :integer
+#  user_id      :integer
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
 #
-
 
 class Comment < ApplicationRecord
   validates :comment_body, presence: true
--- app/models/photo.rb
+++ app/models/photo.rb
@@ -7,7 +7,6 @@
 #  user_id    :integer          not null
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
-#  image      :string
 #
 
 class Photo < ApplicationRecord 
