a92dacd5d620754240967ae1e77649f618c7041a
jeffreytseng3 <jeffrey.tseng3@gmail.com>
2019-11-05 14:26:12 -0500

    tryout new auth params

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,13 +7,20 @@ class UsersController < ApplicationController
   end
 
   def create
-  
-    @user = User.new(user_params)
-    if @user.save
-      session[:user_id] = @user.id
+    # @user = User.new(user_params)
+    # if @user.save
+    #   session[:user_id] = @user.id
+    #   render status: :created
+    # else
+    #   render json: { errors: @user.errors }, status: :unprocessable_entity
+    # end
+      if @user.save
+ 
+      login!(@user)
       render status: :created
     else
-      render json: { errors: @user.errors }, status: :unprocessable_entity
+    
+      render json: @user.errors.full_messages, status: 422
     end
   end
 
